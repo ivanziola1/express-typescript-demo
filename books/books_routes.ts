0ffbd2dcb59e3a1f10a1f574c71f8c27f340cd07
Ivan Zola <ivan.ziola@gmail.com>
2021-06-05 22:09:00 +0300

    add logs, cors and basic routing

---
+++ books/books_routes.ts
@@ -0,0 +1,40 @@
+import { CommonRoutesConfig } from "../common/common_routes_config";
+import { Application, Request, Response, NextFunction } from "express";
+
+export class BooksRoutes extends CommonRoutesConfig {
+  books: { id: number; name?: string }[] = [
+    { id: 1, name: "Don Quixote" },
+    { id: 2, name: "Lord of the Rings" },
+    { id: 3, name: "Harry Potter" },
+  ];
+
+  constructor(app: Application) {
+    super(app, "BooksRoutes");
+  }
+
+  configureRoutes(): Application {
+    this.app
+      .route(`/books`)
+      .get(async (req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send(this.books);
+      })
+      .post(async (req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send({});
+      });
+
+    this.app
+      .route(`/books/:bookId`)
+      .all((req: Request, res: Response, next: NextFunction) => {
+        // auth ?
+        next();
+      })
+      .get(async (req: Request, res: Response): Promise<Response> => {
+        const book = this.books.find(
+          (el) => el.id == Number(req.params.bookId)
+        );
+        return res.status(200).send(book);
+      });
+
+    return this.app;
+  }
+}
--- index.ts
+++ index.ts
@@ -1,26 +1,47 @@
 import express, { Application, Request, Response } from "express";
+import { format, transports } from "winston";
+import { logger, LoggerOptions } from "express-winston";
+import cors from "cors";
+// import debug from "debug";
+import { CommonRoutesConfig } from "./common/common_routes_config";
+import { BooksRoutes } from "./books/books_routes";
 
 const app: Application = express();
 const port = 3000;
+const routes: Array<CommonRoutesConfig> = [];
+// const debugLog: debug.IDebugger = debug("app");
 
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get(
-    "/",
-    async (req: Request, res: Response): Promise<Response> => {
-        return res.status(200).send({
-            message: "Hello World!",
-        });
-    }
-);
+const loggerOptions: LoggerOptions = {
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.json(),
+    format.prettyPrint(),
+    format.colorize({ all: true })
+  ),
+};
 
-try {
-    app.listen(port, (): void => {
-        console.log(`Connected successfully on port ${port}`);
-    });
-} catch (error) {
-    console.error(`Error occured: ${error.message}`);
+if (!process.env.DEBUG) {
+  loggerOptions.meta = false; // when not debugging, log requests as one-liners
 }
 
+// register Logger
+app.use(logger(loggerOptions));
+// register Books routes
+routes.push(new BooksRoutes(app));
+
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: "Hello World!",
+  });
+});
+
+try {
+  app.listen(port);
+} catch (error) {
+  console.error(`Error occured: ${error.message}`);
+}
